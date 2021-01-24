55141b577e9c7ee798516cb58c9c8878381d4346
Gabriel Henrique Vilibor <gh_vili@hotmail.com>
2021-01-24 14:58:58 -0300

    Created ProdutosImagem

---
+++ src/main/java/com/example/domain/ProdutosImagem.java
@@ -0,0 +1,43 @@
+package com.example.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity(name = "produto_imagem")
+public class ProdutosImagem implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100, nullable = false)
+	private String nome;
+	
+	@Column(columnDefinition = "text", nullable = false)
+	private String localizacao;
+	
+	@OneToOne
+	@JoinColumn(name = "id_produto")
+	private Produto produto;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private Usuario usuario;
+
+}
